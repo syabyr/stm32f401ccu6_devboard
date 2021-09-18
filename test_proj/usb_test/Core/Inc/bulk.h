554722ec3aff891adadf8c8b373c1fb5e3d52fec
syabyr <mybays@sina.com>
2021-09-18 15:11:58 +0800

    format

---
+++ test_proj/usb_test/Core/Inc/bulk.h
@@ -0,0 +1,16 @@
+#ifndef __BULK_H
+#define __BULK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+uint32_t bulk_read(uint8_t *pBuf, uint32_t buf_len);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BULK_H */
